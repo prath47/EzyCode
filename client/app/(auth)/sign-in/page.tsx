06f60416e69c171a662588be0f217a653079a2ce
Prath <prathme47@gmail.com>
2026-04-07 00:34:55 +0530

    feat: implement authentication system using better-auth with Prisma and GitHub provider

---
+++ client/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from '@/components/login-form'
+const Page = () => {
+  return (
+    <>
+        <LoginForm />
+    </>
+  )
+}
+
+export default Page
--- server/src/lib/db.js
+++ server/src/lib/db.js
@@ -1,7 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
 
 const globalForPrisma = global;
-const prisma = new PrismaClient();
+
+const prisma = new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
